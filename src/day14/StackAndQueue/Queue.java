e309df35d365152385c9db9915c368f57b40b350
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-04 00:51:12 +0530

    Ability to Create Queue

---
+++ src/day14/StackAndQueue/Queue.java
@@ -0,0 +1,41 @@
+package day14.StackAndQueue;
+
+public class Queue {
+	MyNode head;
+
+	/*
+	 * 1. created enqueue Method to add elements in the Queue
+	 */
+	public void enqueue(int data) {
+		/*
+		 * Created node Object of MyNode class
+		 */
+		MyNode node = new MyNode(data);
+
+		/*
+		 * If Queue is empty, head will point to the node
+		 */
+		if (head == null) {
+			head = node;
+		} else {
+			MyNode temp = head;
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = node;
+		}
+
+	}
+
+	/*
+	 * Created Method PrintList to Print OutPut
+	 */
+	public void printList() {
+		MyNode temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + "->");
+			temp = temp.next;
+		}
+		System.out.println("");
+	}
+}
--- src/day14/StackAndQueue/StackMain.java
+++ src/day14/StackAndQueue/StackMain.java
@@ -3,48 +3,34 @@ package day14.StackAndQueue;
 public class StackMain {
 
 	public static void main(String[] args) {
-
-		/*
-		 * PROCEDURE :
+		/**
+		 * Main method for manipulation Queue Operation
 		 * 
-		 * 1.creating an Oject of class Stack
-		 * 2.Calling method to push value to stack
-		 * 3.calling method to display value of stack
+		 * @param args - Default Java param (Not used)
+		 */
+
+		/**
+		 * PROCEDURE
+		 * 1.creating Object 
+		 * 2.Calling method to add value at first
+		 * 3.calling method to display value of queue
 		 */
 
 		/*
-		 * deleting stack head element
+		 * 1.creating Object an Oject
 		 */
-		System.out.println("deleting element ::");
+		Queue queue = new Queue();
 		/*
-		 * 1.creating an Oject of class Stack
+		 * 2.Calling method to add
 		 */
-		Stack stack = new Stack();
+		queue.enqueue(56);
+		queue.enqueue(30);
+		queue.enqueue(70);
 
 		/*
-		 * Calling mathod addNode to add elements
+		 * 3.calling method to display value of Queue
 		 */
-		stack.addNode(70);
-		stack.addNode(30);
-		stack.addNode(56);
-
-		/*
-		 * Calling Method Peak it will returns the value of what is on the top of the
-		 * stack
-		 */
-		stack.peak();
-
-		/*
-		 * pop: Created Method deleteHead will remove the value of the stack and then
-		 * return it
-		 */
-		stack.deleteHead();
-		stack.printList();
-		stack.peak();
-		stack.deleteHead();
-		stack.printList();
-		stack.peak();
-		stack.deleteHead();
-
+		queue.printList();
 	}
+
 }
